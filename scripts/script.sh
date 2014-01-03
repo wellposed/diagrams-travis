84d07beeb3ac071532dc688f5eca7d446d1f7c13
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2014-01-03 02:53:27 -0500

    woops, bash syntax is weird. bad multine quoting

--- scripts/script.sh
+++ scripts/script.sh
@@ -9,6 +9,6 @@ fi
 
 $CABAL configure --enable-tests --enable-benchmarks -v2 $CABAL_FLAGS\
   && $CABAL build --ghc-options='-Wall -Werror'\
-  && $CABAL test\  # removed $docbuildline 
+  && $CABAL test\  
   && $CABAL check\
   && $CABAL sdist
