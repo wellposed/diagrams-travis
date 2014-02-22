c7795cf32a7336b32db1c5ec8518b30d0e76e7cd
Carter Tazio Schonwald <carter.schonwald@gmail.com>
2014-02-22 17:26:43 -0500

    trying to kill werror

--- scripts/script.sh
+++ scripts/script.sh
@@ -8,7 +8,7 @@ if [[ $GHCVER == "head" || -n "$SKIP_HADDOCK" ]]
 fi
 
 $CABAL configure --enable-tests --enable-benchmarks -v2 $CABAL_FLAGS\
-  && $CABAL build --ghc-options=''\
+  && $CABAL build \
   && $CABAL test\
   && $CABAL check\
   && $CABAL sdist
